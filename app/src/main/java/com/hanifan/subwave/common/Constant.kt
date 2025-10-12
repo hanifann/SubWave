fa5193b4ef3c62f3fa5a98f867ebdf5fac4dba14
Hanifan <hanifannurulhaq@outlook.com>
2025-10-12 15:42:23 +0700

    feat: add common resources module

---
+++ app/src/main/java/com/hanifan/subwave/common/Constant.kt
@@ -0,0 +1,9 @@
+package com.hanifan.subwave.common
+
+object Constant {
+    const val BASE_URL = "https://example.com/"
+    const val SUBSONIC_ROOT_KEY = "subsonic-response"
+    const val DATASTORE_NAME = "subwave_datastore"
+    const val SQLCIPHER_LIB_NAME = "sqlcipher"
+    const val  DATABASE_NAME = "subwave_database.db"
+}
