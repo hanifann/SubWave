7cfc729d4835e79ac83f9781f3bddbe72763465f
hanifan <hanifannnurulhaq@outlook.com>
2026-01-07 08:58:44 +0700

    feat: add user as parameter. return Response

--- app/src/main/java/com/hanifan/subwave/data/login/datasource/LoginRemoteDataSource.kt
+++ app/src/main/java/com/hanifan/subwave/data/login/datasource/LoginRemoteDataSource.kt
@@ -1,14 +1,17 @@
-package com.hanifan.subwave.data.login.data_source
+package com.hanifan.subwave.data.login.datasource
 
 import com.hanifan.subwave.core.network.ResponseDto
 import com.hanifan.subwave.data.login.dto.UserDto
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface LoginRemoteDataSource {
-    @GET("getUser")
+    @GET()
     suspend fun getUser(
+        @Url url: String,
         @Query("u") username: String,
-        @Query("p") password: String
-    ): ResponseDto<UserDto>
+        @Query("p") password: String,
+    ): Response<ResponseDto<UserDto>>
 }
