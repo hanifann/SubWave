31207028f0169bb043b78e39e893a143d5429d2e
hanifan <hanifannnurulhaq@outlook.com>
2026-01-07 09:00:51 +0700

    feat: set imageLoader to crossfade

--- app/src/main/java/com/hanifan/subwave/MainActivity.kt
+++ app/src/main/java/com/hanifan/subwave/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.request.crossfade
 import com.hanifan.subwave.navigation.AppNavigation
 import com.hanifan.subwave.ui.theme.SubWaveTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +15,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        SingletonImageLoader.setSafe {
+            ImageLoader.Builder(it)
+                .crossfade(true)
+                .build()
+        }
+
         enableEdgeToEdge()
         setContent {
             SubWaveTheme {
