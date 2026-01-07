d334c9e82d69cc2eb2f0814edde5da048c3e0868
hanifan <hanifannnurulhaq@outlook.com>
2026-01-07 09:02:55 +0700

    refactor: use withContext on return

--- app/src/main/java/com/hanifan/subwave/core/network/SafeApiCall.kt
+++ app/src/main/java/com/hanifan/subwave/core/network/SafeApiCall.kt
@@ -2,20 +2,22 @@ package com.hanifan.subwave.core.network
 
 import com.hanifan.subwave.common.Resource
 import com.hanifan.subwave.utils.extension.ensureError
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
 
 suspend inline fun <T, R> safeApiCall(
     crossinline call: suspend () -> ResponseDto<T>,
     crossinline mapper: (T) -> R,
-): Resource<R> {
-    return try {
+): Resource<R> = withContext(Dispatchers.IO){
+    return@withContext try {
         val dto = call()
         val base = dto.toResponse().response
 
-        base.error?.let { return Resource.Error(it.message) }
+        base.error?.let { return@withContext Resource.Error(it.message) }
 
-        val data = base.data ?: return Resource.Error("Empty data")
+        val data = base.data ?: return@withContext Resource.Error("Empty data")
 
         Resource.Success(mapper(data))
     } catch (e: HttpException) {
