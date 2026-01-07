157af70a83536c8b82c05a5429edacf0df7fefe4
hanifan <hanifannnurulhaq@outlook.com>
2026-01-07 09:02:19 +0700

    feat: implement temporary home view

--- app/src/main/java/com/hanifan/subwave/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/hanifan/subwave/ui/screen/home/HomeScreen.kt
@@ -10,56 +10,121 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.hanifan.subwave.domain.home.enum.SongCategory
+import com.hanifan.subwave.domain.home.model.Album
 import com.hanifan.subwave.navigation.Routes
 import com.hanifan.subwave.ui.component.ContainerSongInfo
 import com.hanifan.subwave.ui.component.SectionHeader
 
 @Composable
 fun HomeScreen(
-    onNavigate: (route: Routes) -> Unit
+    homeViewModel: HomeViewModel = hiltViewModel(),
+    onNavigate: (Routes) -> Unit,
 ) {
+    val mostPlayedState by homeViewModel.mostPlayedState.collectAsStateWithLifecycle()
+    val recommendedState by homeViewModel.recommendedAlbumState.collectAsStateWithLifecycle()
+    val newestState by homeViewModel.newestAlbumState.collectAsStateWithLifecycle()
+    val recentlyAddedState by homeViewModel.recentlyAddedAlbumState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = Unit) {
+        homeViewModel.getTopAlbum()
+        homeViewModel.getRecommendedAlbum()
+        homeViewModel.getRecentlyAddedAlbum()
+        homeViewModel.getRecentlyPlayedAlbum()
+    }
+
     Scaffold { contentPadding ->
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = contentPadding.calculateTopPadding())
                 .verticalScroll(rememberScrollState())
         ) {
-            SongSection(
-                title = "Recommended",
-                songTitle = "Scene six: through her eyes",
-                singer = "Dream Theatre",
-                albumArt = "https://picsum.photos/400"
-            ) {
-                onNavigate(Routes.SectionDetailRoute)
-            }
-            SongSection(
-                title = "Recently Played",
-                songTitle = "Scene six: through her eyes",
-                singer = "Dream Theatre",
-                albumArt = "https://picsum.photos/400"
-            ) {
+            when {
+                mostPlayedState.isLoading -> {
+                    CircularProgressIndicator()
+                }
 
-            }
-            SongSection(
-                title = "Most Played",
-                songTitle = "Scene six: through her eyes",
-                singer = "Dream Theatre",
-                albumArt = "https://picsum.photos/400"
-            ) {
+                mostPlayedState.errorMessage.isNotBlank() -> {
+                    Text(mostPlayedState.errorMessage)
+                }
 
+                mostPlayedState.data.isNotEmpty() -> {
+                    SongSection(
+                        title = SongCategory.MOSTPLAYED.displayName,
+                        data = mostPlayedState.data,
+                        coverArtUrl = homeViewModel::buildCoverArtUrl,
+                    ) {
+                        onNavigate(Routes.AlbumRoute)
+                    }
+                }
             }
-            SongSection(
-                title = "Recommended",
-                songTitle = "Scene six: through her eyes",
-                singer = "Dream Theatre",
-                albumArt = "https://picsum.photos/400"
-            ) {
+            when {
+                newestState.isLoading -> {
+                    CircularProgressIndicator()
+                }
 
+                newestState.errorMessage.isNotBlank() -> {
+                    Text(newestState.errorMessage)
+                }
+
+                newestState.data.isNotEmpty() -> {
+                    SongSection(
+                        title = SongCategory.RECENTLYADDED.displayName,
+                        data = newestState.data,
+                        coverArtUrl = homeViewModel::buildCoverArtUrl,
+                    ) {
+                        onNavigate(Routes.AlbumRoute)
+                    }
+                }
+            }
+            when {
+                recentlyAddedState.isLoading -> {
+                    CircularProgressIndicator()
+                }
+
+                recentlyAddedState.errorMessage.isNotBlank() -> {
+                    Text(recentlyAddedState.errorMessage)
+                }
+
+                recentlyAddedState.data.isNotEmpty() -> {
+                    SongSection(
+                        title = SongCategory.RECENTLYPLAYED.displayName,
+                        data = recentlyAddedState.data,
+                        coverArtUrl = homeViewModel::buildCoverArtUrl,
+                    ) {
+                        onNavigate(Routes.AlbumRoute)
+                    }
+                }
+            }
+            when {
+                recommendedState.isLoading -> {
+                    CircularProgressIndicator()
+                }
+
+                recommendedState.errorMessage.isNotBlank() -> {
+                    Text(recommendedState.errorMessage)
+                }
+
+                recommendedState.data.isNotEmpty() -> {
+                    SongSection(
+                        title = SongCategory.RECOMMENDED.displayName,
+                        data = recommendedState.data,
+                        coverArtUrl = homeViewModel::buildCoverArtUrl,
+                    ) {
+                        onNavigate(Routes.AlbumRoute)
+                    }
+                }
             }
             Spacer(
                 modifier = Modifier
@@ -72,11 +137,9 @@ fun HomeScreen(
 @Composable
 private fun SongSection(
     title: String,
-    songTitle: String,
-    singer: String,
-    albumArt: String,
+    data: List<Album>,
+    coverArtUrl: (String)-> String,
     onClick: () -> Unit,
-
 ) {
     SectionHeader(
         title = title,
@@ -87,11 +150,11 @@ private fun SongSection(
         contentPadding = PaddingValues(horizontal = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
     ) {
-        items(10) {
+        items(data.size) {index ->
             ContainerSongInfo(
-                songTitle = songTitle,
-                singer = singer,
-                albumArt = albumArt,
+                songTitle = data[index].title,
+                singer = data[index].artist,
+                albumArt = coverArtUrl(data[index].coverArt),
                 onClick = onClick,
             )
         }
